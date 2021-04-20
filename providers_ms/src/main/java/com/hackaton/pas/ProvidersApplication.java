d7ef0117ef644448271bbce4a1cbcb8e7be31e7e
Liu <xxliu95@gmail.com>
2021-04-20 14:12:30 +0200

    Medicos_ms

--- providers_ms/src/main/java/com/hackaton/pas/ProvidersApplication.java
+++ providers_ms/src/main/java/com/hackaton/pas/ProvidersApplication.java
@@ -1,3 +1,4 @@
+package com.hackaton.pas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
